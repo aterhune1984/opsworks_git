df03782cd28ed56a42720d5d2f535c5bc119fcf3
andy.terhune <andy.terhune@rackspace.com>
2016-12-31 11:17:58 -0600

    fixing recipe

--- helloworld_cookbook/recipes/default.rb
+++ helloworld_cookbook/recipes/default.rb
@@ -11,7 +11,7 @@ package "git" do
   options "--force-yes" if node["platform"] == "ubuntu" && node["platform_version"] == "14.04"
 end
 
-git #{app_path} do
+git "#{app_path}" do
     repository app["app_source"]["url"]
     revision app["revision"]["generic"]
     action :sync
