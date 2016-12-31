a7ac709b330e8bffcaae05bdce4c73f003e6dea8
andy.terhune <andy.terhune@rackspace.com>
2016-12-31 10:46:55 -0600

    modifying defaul helloworld cookbook

--- helloworld_cookbook/recipes/default.rb
+++ helloworld_cookbook/recipes/default.rb
@@ -1,1 +1,18 @@
-Chef::Log.info("*****Running Default Recipe in helloworld_cookbook*****")
+app = search(:aws_opsworks_app).first
+app_path = "/srv/#{app['shortname']}"
+Chef::Log.info("app is ... #{app}")
+Chef::Log.info("apppath is ... #{app_path}")
+
+package "git" do
+  # workaround for:
+  # WARNING: The following packages cannot be authenticated!
+  # liberror-perl
+  # STDERR: E: There are problems and -y was used without --force-yes
+  options "--force-yes" if node["platform"] == "ubuntu" && node["platform_version"] == "14.04"
+end
+
+include_recipe 'poise-python'
+
+application app_path do
+    Chef::Log.info("inside application do statement")
+end
