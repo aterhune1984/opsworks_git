54a1a9494e001bc331d51f67febf1ddd5c9cda93
andy.terhune <andy.terhune@rackspace.com>
2016-12-31 16:24:47 -0600

    adding files

---
+++ helloworld_cookbook/files/helloworld.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# chkconfig: 345 99 01
+# description: some startup script
+
+case "$1" in
+start)
+   /home/ec2-user/pythondemo/bin/python /srv/pythondemo/helloworld.py &
+   echo $!>/var/run/hello.pid
+   ;;
+stop)
+   kill -9 `cat /var/run/hello.pid`
+   rm /var/run/hello.pid
+   ;;
+restart)
+   $0 stop
+   $0 start
+   ;;
+status)
+   if [ -e /var/run/hello.pid ]; then
+      echo helloworld.sh is running, pid=`cat /var/run/hello.pid`
+   else
+      echo helloworld.sh is NOT running
+      exit 1
+   fi
+   ;;
+*)
+   echo "Usage: $0 {start|stop|status|restart}"
+esac
+
+exit 0
--- helloworld_cookbook/recipes/default.rb
+++ helloworld_cookbook/recipes/default.rb
@@ -3,6 +3,7 @@ app_path = "/srv/#{app['shortname']}"
 Chef::Log.info("app is ... #{app}")
 Chef::Log.info("apppath is ... #{app_path}")
 
+# install the git package
 package "git" do
   # workaround for:
   # WARNING: The following packages cannot be authenticated!
@@ -11,11 +12,14 @@ package "git" do
   options "--force-yes" if node["platform"] == "ubuntu" && node["platform_version"] == "14.04"
 end
 
+# sync the app repo to the app_path
 git "#{app_path}" do
     repository app["app_source"]["url"]
     revision app["app_source"]["revision"]
     action :sync
 end
+
+# create a virtual environment if it does not exist
 
 bash "Create a virtual environment" do
   user "ec2-user"
@@ -31,4 +35,20 @@ bash "Create a virtual environment" do
   not_if do
       File.exists?("/home/ec2-user/#{app['shortname']}/bin/activate")
   end
+end 
+
+cookbook_file "Copy a file" do
+    group "root"
+    mode "0755"
+    owner "root"
+    path "/etc/init.d/helloworld"
+    source "helloworld.sh"
+end
+
+execute "add to chkconfig" do
+  command "chkconfig --add helloworld"
+end 
+
+service 'helloworld' do
+    action :start
 end 
